90a13797a37341c478a51f8b74df673a17cd7272
HalilCan <halilcanmemoglu@gmail.com>
2017-11-02 15:00:02 -0400

    Char <-> ASCII fixed

--- learnyounode/io1.js
+++ learnyounode/io1.js
@@ -7,17 +7,17 @@ var fileBuffer = fs.readFileSync(filepath);
 var index = 0;
 var bufferReduce = function (buffer, callback, accumulator) {
 	index += 1;
-	if (buffer.length = 1) {
-		return function(accumulator, buffer[0], index, accumulator);
+	if (buffer.legnth === 0) {
+		return accumulator;
+	} else if (buffer.length === 1) {
+		return callback(accumulator, buffer[0]);
 		index = 0;
-	} else {
-		return bufferReduce(buffer.slice(1), callback, callback(accumulator, buffer[0], index - 1, buffer));
-	}
+	} else return bufferReduce(buffer.slice(1), callback, callback(accumulator, buffer[0]));
 }
 
-function specificCharCount (array, char) {
+var specificCharCount = function (array, char) {
 	return bufferReduce(array, function(acc, val){
-		if (val === '\n') {
+		if (String.fromCharCode(val) == char) {
 			return acc+1;
 		} else {
 			return acc;
@@ -25,4 +25,4 @@ function specificCharCount (array, char) {
 	}, 0);
 }
 
-console.log()
+console.log(specificCharCount(fileBuffer, '\n'));
