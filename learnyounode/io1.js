aecc13003f0aa953cad2c621764899fc67e78676
HalilCan <halilcanmemoglu@gmail.com>
2017-11-02 14:43:15 -0400

    Fixed declaration

--- learnyounode/io1.js
+++ learnyounode/io1.js
@@ -4,16 +4,19 @@ var arguments = process.argv;
 var filepath = arguments[2];
 var fileBuffer = fs.readFileSync(filepath);
 
-function bufferReduce (buffer, function, accumulator) {
+var index = 0;
+var bufferReduce = function (buffer, callback, accumulator) {
+	index += 1;
 	if (buffer.length = 1) {
-		return function(buffer[1]);
+		return function(accumulator, buffer[0], index, accumulator);
+		index = 0;
 	} else {
-		return bufferReduce(buffer.slice(1), function, function(buffer[1], accumulator));
+		return bufferReduce(buffer.slice(1), callback, callback(accumulator, buffer[0], index - 1, buffer));
 	}
 }
 
 function specificCharCount (array, char) {
-	return bufferReduce(array, function(val, acc){
+	return bufferReduce(array, function(acc, val){
 		if (val === '\n') {
 			return acc+1;
 		} else {
